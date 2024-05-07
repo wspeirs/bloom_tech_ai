b7270bcb5147c8c26320f502cb4b5ba361762eda
William Speirs <bill.speirs@gmail.com>
2024-05-07 09:48:39 -0400

    Reorg repo for chat bot project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bloom_tech_ai
 Work related to the Bloom Tech AI course
+
+# Polars Chat Bot
+A chat bot that uses RAG to answer questions from the pola.rs documentation
--- day2_prompting.py
+++
@@ -1,4 +0,0 @@
-# done in co-lab: https://colab.research.google.com/drive/1VqqGeHxEDb3T3NIh6ofkkKoeS_yHHxIn?usp=sharing
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_openai import ChatOpenAI, OpenAI
-
